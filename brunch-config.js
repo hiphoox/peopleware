ebb8234cd6c1acb14e584df33aeeb5428aeda475
Giovanni Cortes <me@giovannicortes.com>
2015-09-28 14:12:11 -0500

    Tel, Cel and Salary accept only numbers

--- brunch-config.js
+++ brunch-config.js
@@ -2,13 +2,13 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: 'js/app.js'
+      joinTo: 'js/app.js',
     },
     stylesheets: {
       joinTo: 'css/app.css'
     },
     templates: {
-      joinTo: 'js/app.js'
+      joinTo: 'js/app.js',
     }
   },
 
