92e413420bb15d17a6c347830fe38894e2281113
Giovanni Cortes <me@giovannicortes.com>
2015-10-21 17:33:51 -0500

    Fixed bug migration

--- priv/repo/migrations/20151021222919_update_last_salary.exs
+++
@@ -1,7 +0,0 @@
-defmodule Peopleware.Repo.Migrations.UpdateLastSalary do
-  use Ecto.Migration
-
-  def change do
-    execute "ALTER TABLE public.profiles ALTER COLUMN last_salary TYPE varchar(12);"
-  end
-end
