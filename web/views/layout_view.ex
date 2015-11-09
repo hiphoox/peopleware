7d4e7204323fcb50d93d00ab7f9e5ad0c298f68a
Giovanni Cortes <me@giovannicortes.com>
2015-11-09 14:45:30 -0600

    Styles to range input

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,6 +1,9 @@
 defmodule Peopleware.LayoutView do
   use Peopleware.Web, :view
 
+  @doc """
+  Get a path from the @conn struct
+  """
   def get_path(conn) do
     %{request_path: path} = conn
     path
