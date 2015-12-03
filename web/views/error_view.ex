52f30913d1bc26b6b689ce9d60308e1404830719
Giovanni Cortes <me@giovannicortes.com>
2015-12-03 15:00:15 -0600

    Error 404 template

--- web/views/error_view.ex
+++ web/views/error_view.ex
@@ -2,15 +2,16 @@ defmodule Peopleware.ErrorView do
   use Peopleware.Web, :view
 
   def render("404.html", _assigns) do
-    "Page not found - 404"
+    render("not_found.html", %{})
   end
 
   def render("500.html", _assigns) do
     "Server internal error - 500"
   end
 
-  # Render all other templates as 500
-  def render(_, assigns) do
+  # In case no render clause matches or no
+  # template is found, let's render it as 500
+  def template_not_found(_template, assigns) do
     render "500.html", assigns
   end
 end
